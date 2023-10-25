0d7623a5d4194a700d39c6d0786e160265a486ea
amrutajavir <95489538+amrutajavir@users.noreply.github.com>
2023-10-25 12:38:30 +0530

    Add files via upload

---
+++ Data.jsx
@@ -0,0 +1,29 @@
+const Data =[
+    {id:10, name:"Alia Bhatt",
+    age:"29",
+    image:
+    "https://tse3.mm.bing.net/th?id=OIP.DmRsoWQDdccHHh4m0eghNwHaEK&pid=Api&P=0&h=180"},
+
+    {id:20, name: "Shubhaman Gill", age:"27",
+    image:
+    "https://hoistore.in/wp-content/uploads/2021/01/shubman-gill-4.jpg"},
+
+    {id:30, name:"Atif Aslam", age:"40",
+    image:
+    "https://dailytimes.tv/wp-content/uploads/2021/08/Atif-Aslam.jpg"},
+
+    {id:40, name: "Vijay Devrakonda", age:"36",
+     image:
+    "https://2.bp.blogspot.com/-f6Xj0Xqjoms/XFV01HJ-22I/AAAAAAAAVbU/PzAN-ypm7UY7EuWDwg87amhg8I8PGvoYQCLcBGAs/s1600/Vijay%2BDeverakonda%2Bgo%2Bprofile%2B2.jpg"},
+    
+    {id:50, name:"Kareena Kapoor", age:"43",
+     image:
+     "https://images.hdqwalls.com/wallpapers/kareena-kapoor-2016-lu.jpg"}
+    
+]
+
+
+
+
+
+export default Data
